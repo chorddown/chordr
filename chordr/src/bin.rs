5daafe6d34f2e32c0780e89039135d05e52b776a
Daniel Corn <info@cundd.net>
2019-12-14 19:02:26 +0100

    Add support to build a catalog through CLI

--- chordr/src/bin.rs
+++ chordr/src/bin.rs
@@ -1,60 +1,64 @@
 extern crate clap;
 extern crate libchordr;
 
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand, ArgMatches};
 use std::fs;
 
 use libchordr::prelude::*;
-use libchordr::prelude::build_tokenizer;
+use libchordr::prelude::Result;
+use libchordr::prelude::Error;
 
 fn main() {
+    let output_arg = Arg::with_name("OUTPUT")
+        .required(true)
+        .help("Output file name");
     let args = App::new("chordr")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Daniel Corn <info@cundd.net>")
-        .about("Convert chordpro files")
-        .arg(Arg::with_name("OUTPUT")
-            .long("output")
-            .short("o")
-            .takes_value(true)
-            .help("Output file name"))
-        // .arg(Arg::with_name("TITLE")
-        //     .long("title")
-        //     .takes_value(true)
-        //     .help("Title (in metadata) of the output file"))
-        // .arg(Arg::with_name("AUTHOR")
-        //     .long("author")
-        //     .takes_value(true)
-        //     .help("Author (in metadata) of the output file"))
-        // .arg(Arg::with_name("SOURCENAMES")
-        //     .long("sourcenames")
-        //     .help("Show name of chopro source file on page"))
-        // .arg(Arg::with_name("CHORDS")
-        //     .long("chords")
-        //     .help("Add a separate page of chord definitions"))
-        .arg(Arg::with_name("INPUT")
-            .required(true)
-            .multiple(true)
-            .help("Chopro file(s) to parse"))
-        // .after_help(
-        //     "At least one INPUT file is required unless the --chords \
-        //      flag is given.\n\n\
-        //      Each INPUT file contains one or more song in the \
-        //      chopro format, which is described at \
-        //      https://github.com/kaj/chord3/blob/master/chopro.md .",
-        // )
+        .about("Manage chorddown files and catalogs")
+        .subcommand(
+            SubCommand::with_name("convert")
+                .about("Convert chorddown files")
+                .arg(Arg::with_name("INPUT")
+                    .required(true)
+                    .help("Chorddown file to parse"))
+                .arg(output_arg.clone())
+        )
+        .subcommand(
+            SubCommand::with_name("build-catalog")
+                .about("Build a catalog from chorddown files")
+                .arg(Arg::with_name("DIR")
+                    .required(true)
+                    .help("Path to the directory of chorddown files"))
+                .arg(output_arg.clone())
+                .arg(Arg::with_name("pretty")
+                    .long("pretty")
+                    .short("p")
+                    .help("Output indented JSON")
+                )
+        )
         .get_matches();
 
-    let file_path = args.value_of("INPUT").unwrap();
-    let contents = fs::read_to_string(file_path)
-        .expect(&format!("File '{}' could not be read", file_path));
+    let error = if let Some(matches) = args.subcommand_matches("convert") {
+        convert(matches)
+    } else if let Some(matches) = args.subcommand_matches("build-catalog") {
+        build_catalog(matches)
+    } else {
+        Ok(())
+    };
 
+    if let Err(error) = error {
+        eprintln!("{}", error)
+    }
+}
+
+fn convert(args: &ArgMatches) -> Result<()> {
+    let input_file_path = args.value_of("INPUT").unwrap();
+    let contents = fs::read_to_string(input_file_path)?;
     let tokens = build_tokenizer().tokenize(&contents);
-
-    // token_lines_to_tokens
-    let node = Parser::new().parse(token_lines_to_tokens(tokens));
-    let converted = Converter::new().convert(&node, Format::HTML).unwrap();
-
-    println!(
+    let parser_result = Parser::new().parse(token_lines_to_tokens(tokens));
+    let converted = Converter::new().convert(parser_result.node_as_ref(), Format::HTML)?;
+    let output = format!(
         r#"
 <!DOCTYPE html>
 <html lang="en">
@@ -63,14 +67,46 @@ fn main() {
     <title>{title}</title>
     <style>{styles}</style>
 </head>
-<body class="chordr">
+<body>
 <main>
 {content}
 </main>
 </body>
 </html>
     "#,
-        title = "",
-        styles = include_str!("../resources/html/main.css"),
+        title = parser_result.meta().title.unwrap_or("".to_owned()),
+        styles = include_str!("../../webchordr/static/stylesheets/chordr-default-styles.css"),
         content = converted);
+
+    handle_output(args, output)
+}
+
+fn build_catalog(args: &ArgMatches) -> Result<()> {
+    let dir_path = args.value_of("DIR").unwrap();
+    let pretty = args.is_present("pretty");
+
+    let catalog = CatalogBuilder::new().build_catalog_for_directory(dir_path, FileType::Chorddown, true)?;
+
+    let serialization_result = if pretty {
+        serde_json::to_string_pretty(&catalog)
+    } else {
+        serde_json::to_string(&catalog)
+    };
+
+    let output = match serialization_result {
+        Ok(s) => s,
+        Err(e) => return Err(Error::unknown_error(format!("{}", e)))
+    };
+
+    handle_output(args, output)
+}
+
+fn handle_output(args: &ArgMatches, output: String) -> Result<(), Error> {
+    let output_file_path = args.value_of("OUTPUT").unwrap();
+    if output_file_path == "-" {
+        println!("{}", output);
+        Ok(())
+    } else {
+        Ok(fs::write(output_file_path, output)?)
+    }
 }
