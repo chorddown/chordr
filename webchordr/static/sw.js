add566bc7cf1286b70e71f006c04dc0b72183531
Daniel Corn <info@cundd.net>
2019-12-28 22:38:35 +0100

    Update the chord and app styles and icons

--- webchordr/static/sw.js
+++ webchordr/static/sw.js
@@ -1,6 +1,7 @@
+const CACHE_NAME = 'chordr';
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open('chordr').then(function (cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll([
                 '/',
                 '/index.html',
@@ -15,14 +16,65 @@ self.addEventListener('install', function (e) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    console.log(event.request.url);
+const fetchFromServer = function (event) {
+    return fetch(event.request).then(
+        function (response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+            }
 
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
+            // Stash copy of response
+            const cachedResponse = response.clone();
+            caches.open(CACHE_NAME)
+                .then(function (cache) {
+                    cache.put(event.request, cachedResponse).then();
+                });
+
+            return response;
+        }
     );
+};
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (!response) {
+                    // If not, fetch request, and then cache response
+                    return fetchFromServer(event);
+                } else {
+                    if (navigator.onLine) {
+                        fetchFromServer(event).then(function () {
+                            console.info('INFO:webchordr -- Did load ' + event.request.url + ' in background');
+                        }).catch(function (e) {
+                            console.error('ERROR:webchordr -- Failed loading ' + event.request.url + ' in background', e);
+                        });
+                    }
+                    // If already cached
+                    return response;
+                }
+            })
+//         fetch(event.request).catch(function () {
+//             return caches.match(event.request);
+//         })
+    );
+    // let catalogRequest = /\/catalog\.json/.test(event.request.url);
+    // if (catalogRequest) {
+    //     console.log('[SW] Try to fetch new catalog and fall back to cache', event.request.url);
+    //     event.respondWith(
+    //         fetch(event.request).catch(function () {
+    //             return caches.match(event.request);
+    //         })
+    //     );
+    // } else {
+    //     console.log('[SW] Query cache and fall back to fetch', event.request.url);
+    //
+    //     event.respondWith(
+    //         caches.match(event.request).then(function (response) {
+    //             return response || fetch(event.request);
+    //         })
+    //     );
+    // }
 });
 
 self.addEventListener('activate', function (event) {
