7bdeb46e8cfc24eb9cb41da7f99e607d1accfe94
Daniel Corn <info@cundd.net>
2020-06-06 19:27:25 +0200

    Add Settings Events

--- webchordr/src/events/mod.rs
+++ webchordr/src/events/mod.rs
@@ -1,7 +1,9 @@
 pub mod setlist_events;
+pub mod settings_events;
 pub mod sorting_change;
 
 pub use self::setlist_events::SetlistEvent;
+pub use self::settings_events::SettingsEvent;
 pub use self::sorting_change::SortingChange;
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +15,7 @@ pub trait EventTrait {}
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Event {
     SortingChange(SortingChange),
+    SettingsEvent(SettingsEvent),
     SetlistEvent(SetlistEvent),
 }
 
@@ -25,5 +28,11 @@ impl From<SortingChange> for Event {
 impl From<SetlistEvent> for Event {
     fn from(s: SetlistEvent) -> Self {
         Event::SetlistEvent(s)
+    }
+}
+
+impl From<SettingsEvent> for Event {
+    fn from(s: SettingsEvent) -> Self {
+        Event::SettingsEvent(s)
     }
 }
