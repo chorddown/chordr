cac0edae673cb5d2d3e1da35a1c75cfdb65eeffb
Daniel Corn <info@cundd.net>
2020-06-07 14:00:10 +0200

    Remove unused file

--- libchordr/src/models/setlist_entry.rs
+++
@@ -1,7 +0,0 @@
-use crate::models::prelude::SongData;
-
-#[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
-pub struct SetlistEntry<S: SongData> {
-    song_data: S,
-    song_settings: SongSettings,
-}
