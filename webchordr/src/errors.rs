5184534c99d8f52cf2618529b6d17a5e34e49b57
Daniel Corn <info@cundd.net>
2020-09-12 18:58:28 +0200

    Add additional error types

--- webchordr/src/errors.rs
+++ webchordr/src/errors.rs
@@ -1,13 +1,16 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result};
 use wasm_bindgen::JsValue;
+use web_sys::Response;
 
 #[derive(Debug, Clone)]
 pub enum WebError {
     SortableError(String),
     JsError(String),
+    CustomError(String),
     SetlistDeserializeError(String),
     PersistenceError(PersistenceError),
+    ResponseError(String, Response),
 }
 
 #[allow(unused)]
@@ -20,12 +23,20 @@ impl WebError {
         WebError::JsError(s.into())
     }
 
+    pub fn custom_error<S: Into<String>>(s: S) -> Self {
+        WebError::CustomError(s.into())
+    }
+
     pub fn persistence_error(s: PersistenceError) -> Self {
         WebError::PersistenceError(s)
     }
 
     pub fn setlist_deserialize_error<S: Into<String>>(s: S) -> Self {
         WebError::SetlistDeserializeError(s.into())
+    }
+
+    pub fn response_error<S: Into<String>>(url: S, response: Response) -> Self {
+        WebError::ResponseError(url.into(), response)
     }
 }
 
@@ -34,8 +45,16 @@ impl Display for WebError {
         match self {
             WebError::SortableError(s) => f.write_str(s),
             WebError::JsError(s) => f.write_str(s),
+            WebError::CustomError(s) => f.write_str(s),
             WebError::SetlistDeserializeError(s) => f.write_str(s),
             WebError::PersistenceError(s) => f.write_str(s.to_string().as_str()),
+            WebError::ResponseError(u, r) => write!(
+                f,
+                "Error fetching URL {}: Status {} {}",
+                u,
+                r.status(),
+                r.status_text()
+            ),
         }
     }
 }
