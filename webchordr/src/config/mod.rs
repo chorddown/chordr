58a4674046079e02cf0cf63e389a9e46066ad60f
Daniel Corn <info@cundd.net>
2020-11-13 21:42:30 +0100

    Add config object

---
+++ webchordr/src/config/mod.rs
@@ -0,0 +1,18 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Config {
+    api_url: String,
+}
+
+impl Config {
+    pub fn api_url(&self) -> &str {
+        &self.api_url
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            api_url: "http://localhost:9000".to_string(),
+        }
+    }
+}
--- webchordr/src/lib.rs
+++ webchordr/src/lib.rs
@@ -4,6 +4,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 mod app;
 mod components;
+mod config;
 pub mod constants;
 mod data_exchange;
 mod errors;
