fc4a0974f78c8297b2c563674917d73ba7a04603
Daniel Corn <info@cundd.net>
2021-04-09 16:26:50 +0200

    Use Workbox for offline and cache management

--- webchordr/webpack.config.js
+++ webchordr/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const distPath = path.resolve(__dirname, "dist");
@@ -43,6 +44,37 @@ module.exports = (env, argv) => {
       new WasmPackPlugin({
         crateDirectory: ".",
         withTypeScript: true
+      }),
+      new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
+        cacheId: 'net.chordr',
+        exclude: ['static/songs/.*', /songs/, /.*\.scss/],
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+        runtimeCaching: [
+          {
+            urlPattern: /.*/,
+            handler: 'NetworkFirst'
+          },
+          {
+            urlPattern: '/catalog.json',
+            handler: 'NetworkFirst'
+          },
+          {
+            urlPattern: /catalog\.json\?\d+/,
+            handler: 'NetworkOnly'
+          },
+          {
+            urlPattern: /status/,
+            handler: 'NetworkOnly'
+          },
+          {
+            urlPattern: /assets/,
+            handler: 'StaleWhileRevalidate'
+          }
+        ]
       })
     ],
     watch: argv.mode !== 'production'
