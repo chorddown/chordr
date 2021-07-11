04f52d18f04a0eb749c8a17532263a3e0f502b9c
Daniel Corn <info@cundd.net>
2021-07-11 18:38:52 +0200

    Do not force production mode for wasm-pack

--- webchordr/webpack.config.js
+++ webchordr/webpack.config.js
@@ -50,7 +50,6 @@ module.exports = (env, argv) => {
         ]
       }),
       new WasmPackPlugin({
-        forceMode: 'production',
         crateDirectory: ".",
         withTypeScript: true
       }),
