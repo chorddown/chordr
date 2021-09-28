4eceb297885f82de2b703f0666f6b094bb267b08
Daniel Corn <info@cundd.net>
2021-09-28 22:26:33 +0200

    Use chrono date for `CUNDD_BUILD_REVISION`

--- webchordr/build.rs
+++ webchordr/build.rs
@@ -1,7 +1,8 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::Local;
 
 fn main() {
-    if let Ok(n) = SystemTime::now().duration_since(UNIX_EPOCH) {
-        println!("cargo:rustc-env=CUNDD_BUILD_REVISION={}", n.as_secs())
-    }
+    println!(
+        "cargo:rustc-env=CUNDD_BUILD_REVISION={}",
+        Local::now().format("%Y-%m-%d %H:%M:%S")
+    )
 }
