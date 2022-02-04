21b8dbac00fcf42c405f5ced15868736bb520514
Daniel Corn <info@cundd.net>
2022-02-04 22:28:10 +0100

    Use the `Display` implementation of `RecordTrait::Id` to build keys

--- webchordr/persistence/src/storage_key_utility.rs
+++ webchordr/persistence/src/storage_key_utility.rs
@@ -1,6 +1,3 @@
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-
 use cqrs::prelude::RecordTrait;
 
 use crate::persistence_manager::CommandContext;
@@ -16,16 +13,10 @@ pub fn build_combined_key<N: AsRef<str>, K: AsRef<str>>(namespace: &N, key: &K) 
 }
 
 pub fn build_combined_id_key<R: RecordTrait>(context: &CommandContext, id: &R::Id) -> String {
-    fn calculate_hash<T: Hash>(t: &T) -> u64 {
-        let mut s = DefaultHasher::new();
-        t.hash(&mut s);
-        s.finish()
-    }
-
     format!(
         "{}{}{}",
         build_combined_key(&context.namespace, &context.key,),
         SEPARATOR,
-        calculate_hash(&id)
+        id
     )
 }
