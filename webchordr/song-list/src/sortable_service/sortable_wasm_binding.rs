bb0b31e810991b98c98c8af8b7c87a8992649d78
Daniel Corn <info@cundd.net>
2022-02-15 21:24:44 +0100

    Remove unused WASM binding

--- webchordr/song-list/src/sortable_service/sortable_wasm_binding.rs
+++ webchordr/song-list/src/sortable_service/sortable_wasm_binding.rs
@@ -14,16 +14,3 @@ extern "C" {
     #[wasm_bindgen(method)]
     pub fn destroy(this: &SortableWrapper) -> SortableWrapper;
 }
-
-#[cfg(feature = "trunk_build")]
-#[wasm_bindgen(module = "/src-typescript/SortableWrapper-trunk.js")]
-extern "C" {
-    #[derive(Debug)]
-    pub type SortableWrapper;
-
-    #[wasm_bindgen(constructor)]
-    pub fn new(s: &HtmlElement, handler: &Function, options: &JsValue) -> SortableWrapper;
-
-    #[wasm_bindgen(method)]
-    pub fn destroy(this: &SortableWrapper) -> SortableWrapper;
-}
