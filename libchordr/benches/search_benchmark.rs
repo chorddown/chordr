afedce7a4ed577b124de94d3aba2fbb928c2bc74
Daniel Corn <info@cundd.net>
2022-03-20 21:45:18 +0100

    Use `bench_with_input()` for the search benchmark

--- libchordr/benches/search_benchmark.rs
+++ libchordr/benches/search_benchmark.rs
@@ -1,24 +1,33 @@
 use std::rc::Rc;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use libchordr::prelude::SearchIndex;
 use libchordr::test_helpers::get_test_catalog;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let catalog = Rc::new(get_test_catalog());
-    c.bench_function("Search songs in a catalog", |b| {
-        let index = SearchIndex::build_for_catalog(catalog.clone());
-        b.iter(|| {
-            index.search_by_term(black_box("Toni"));
-        })
-    });
+    c.bench_with_input(
+        BenchmarkId::new("Search songs in a catalog", "catalog"),
+        &catalog,
+        |b, c| {
+            let index = SearchIndex::build_for_catalog(c.clone());
+            b.iter(|| {
+                index.search_by_term(black_box("Toni"));
+            })
+        },
+    );
 
-    c.bench_function("Build search-index for catalog", |b| {
-        b.iter(|| {
-            SearchIndex::build_for_catalog(black_box(catalog.clone()));
-        })
-    });
+    let catalog = Rc::new(get_test_catalog());
+    c.bench_with_input(
+        BenchmarkId::new("Build search-index for catalog", "catalog"),
+        &catalog,
+        |b, c| {
+            b.iter(|| {
+                SearchIndex::build_for_catalog(black_box(c.clone()));
+            })
+        },
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
