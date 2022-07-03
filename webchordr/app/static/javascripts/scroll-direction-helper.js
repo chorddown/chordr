46bc84958c95c1d444b75dc37d04b2275bde396d
Daniel Corn <info@cundd.net>
2022-07-03 18:26:27 +0200

    Code formatting

--- webchordr/app/static/javascripts/scroll-direction-helper.js
+++ webchordr/app/static/javascripts/scroll-direction-helper.js
@@ -1,70 +1,69 @@
+const threshold = 5;
+const detachThreshold = 50;
+const classList = document.documentElement.classList;
 
-    const threshold = 5;
-    const detachThreshold = 50;
-    const classList = document.documentElement.classList;
+let lastScrollPosition = 0.0;
+let lastScrollDirection = undefined;
+let isAttached = false;
 
-    let lastScrollPosition = 0.0;
-    let lastScrollDirection = undefined;
-    let isAttached = false;
+/* Initialize */
+export function initialize() {
+    document.addEventListener('DOMContentLoaded', function () {
+        addScrollClasses();
+        window.addEventListener("scroll", throttle(addScrollClasses), {passive: true});
+    });
+}
 
-    /* Initialize */
-    export function initialize() {
-        document.addEventListener('DOMContentLoaded', function () {
-            addScrollClasses();
-            window.addEventListener("scroll", throttle(addScrollClasses), {passive: true});
-        });
+function addScrollClasses() {
+    const currentScrollPosition = window.scrollY;
+    checkIfAttached(currentScrollPosition);
+    checkDirection(currentScrollPosition);
+}
+
+function checkIfAttached(currentScrollPosition) {
+    if (currentScrollPosition < detachThreshold) {
+        if (!isAttached) {
+            classList.add('attached')
+            classList.remove('detached')
+            isAttached = true
+        }
+    } else {
+        if (isAttached) {
+            classList.remove('attached')
+            classList.add('detached')
+            isAttached = false
+        }
     }
+}
 
-    function addScrollClasses() {
-        const currentScrollPosition = window.scrollY;
-        checkIfAttached(currentScrollPosition);
-        checkDirection(currentScrollPosition);
-    }
-
-    function checkIfAttached(currentScrollPosition) {
-        if (currentScrollPosition < detachThreshold) {
-            if (!isAttached) {
-                classList.add('attached')
-                classList.remove('detached')
-                isAttached = true
-            }
+function checkDirection(currentScrollPosition) {
+    let scrollDirection = '';
+    const diff = lastScrollPosition - currentScrollPosition;
+    if (Math.abs(diff) > threshold) {
+        if (diff > 0) {
+            scrollDirection = 'direction-up';
         } else {
-            if (isAttached) {
-                classList.remove('attached')
-                classList.add('detached')
-                isAttached = false
-            }
+            scrollDirection = 'direction-down';
+        }
+
+        if (lastScrollDirection !== scrollDirection) {
+            classList.remove(lastScrollDirection)
+            classList.add(scrollDirection)
+            lastScrollDirection = scrollDirection;
         }
     }
+    lastScrollPosition = currentScrollPosition
+}
 
-    function checkDirection(currentScrollPosition) {
-        let scrollDirection = '';
-        const diff = lastScrollPosition - currentScrollPosition;
-        if (Math.abs(diff) > threshold) {
-            if (diff > 0) {
-                scrollDirection = 'direction-up';
-            } else {
-                scrollDirection = 'direction-down';
-            }
-
-            if (lastScrollDirection !== scrollDirection) {
-                classList.remove(lastScrollDirection)
-                classList.add(scrollDirection)
-                lastScrollDirection = scrollDirection;
-            }
-        }
-        lastScrollPosition = currentScrollPosition
-    }
-
-    function throttle(callback, limit) {
-        let waiting = false;
-        return function () {
-            if (!waiting) {
-                callback.apply(this, arguments);
-                waiting = true;
-                setTimeout(function () {
-                    waiting = false;
-                }, limit || 20);
-            }
+function throttle(callback, limit) {
+    let waiting = false;
+    return function () {
+        if (!waiting) {
+            callback.apply(this, arguments);
+            waiting = true;
+            setTimeout(function () {
+                waiting = false;
+            }, limit || 20);
         }
     }
+}
