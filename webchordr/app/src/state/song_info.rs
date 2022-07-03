b51ce4a0c4157525f37aaff838aae2118b2fc8bd
Daniel Corn <info@cundd.net>
2022-07-03 18:21:24 +0200

    Implement `From<SongInfo>` for `SetlistEntry`

--- webchordr/app/src/state/song_info.rs
+++ webchordr/app/src/state/song_info.rs
@@ -1,10 +1,21 @@
 use yew::prelude::*;
 
-use libchordr::prelude::{Song, SongSettings};
+use libchordr::prelude::{ListEntryTrait, SetlistEntry, Song, SongData, SongSettings};
 
 #[derive(Properties, Clone, Debug, PartialEq)]
 pub struct SongInfo {
     pub song: Song,
     pub song_settings: SongSettings,
     pub is_on_setlist: bool,
+}
+
+impl From<SongInfo> for SetlistEntry {
+    fn from(song_info: SongInfo) -> Self {
+        SetlistEntry::new(
+            song_info.song.id(),
+            song_info.song.file_type(),
+            song_info.song.title(),
+            Some(song_info.song_settings),
+        )
+    }
 }
