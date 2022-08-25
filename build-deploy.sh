8a3c8347a9133131bada7a4b52323042e3176e4f
Daniel Corn <info@cundd.net>
2022-08-25 23:05:50 +0200

    Forward the verbose flag to Trunk

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")" || exit
 
-if ! command -v trunk &> /dev/null; then
+if ! command -v trunk &>/dev/null; then
   echo "[ERROR] Trunk must be installed (https://trunkrs.dev/#install)"
   exit 1
 fi
@@ -24,17 +24,25 @@ echo "[TASK] Build the catalog"
 cargo run --bin chordr -- build-catalog webchordr/app/static/songs webchordr/app/static/catalog.json
 
 echo "[TASK] Create deploy-build"
-pushd webchordr/app || exit 1;
+pushd webchordr/app || exit 1
 
 if [[ $* == *--dev* ]]; then
-  trunk build
+  if [[ $* == *--verbose* ]]; then
+    trunk -v build
+  else
+    trunk build
+  fi
 else
-  trunk build --release
+  if [[ $* == *--verbose* ]]; then
+    trunk -v build --release
+  else
+    trunk build --release
+  fi
 fi
 
 if [[ $* == *--verbose* ]] && type twiggy &>/dev/null; then
   twiggy top -n 10 ./dist/*.wasm
 fi
-popd > /dev/null
+popd >/dev/null
 echo "[TASK] Upload to $1"
 rsync -i --exclude '*.scss' -rzu webchordr/app/dist/ $1
