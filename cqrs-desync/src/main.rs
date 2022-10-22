88ad955ec01837b3165190df643479d7d14c6def
Daniel Corn <info@cundd.net>
2022-10-22 15:25:47 +0200

    Code cleanup

--- cqrs-desync/src/main.rs
+++ cqrs-desync/src/main.rs
@@ -18,7 +18,7 @@ struct Args {
 fn main() {
     if let Err(e) = run() {
         eprintln!("{}", e);
-        if let Some(source) = std::error::Error::source(&e) {
+        if let Some(source) = StdError::source(&e) {
             eprintln!("{}", source);
         }
     }
@@ -57,47 +57,3 @@ fn run() -> Result<(), Error> {
 
     Ok(())
 }
-// fn run() -> Result<(), libcqrs_desync::error::Error> {
-//     let args = Args::parse();
-//
-//     let base_dir = format!("{}/../", env!("CARGO_MANIFEST_DIR"));
-//     let mapping = [
-//         (
-//             format!("{}{}", base_dir, "cqrs/src/async_test.rs"),
-//             format!("{}{}", base_dir, "cqrs/src/sync_test.rs"),
-//         ),
-//         (
-//             format!("{}{}", base_dir, "cqrs/src/async_repository.rs"),
-//             format!("{}{}", base_dir, "cqrs/src/sync_repository.rs"),
-//         ),
-//     ];
-//     let file_reader = FileReader::new();
-//     let file_writer = FileWriter::new();
-//     let code_updater = CodeUpdater::new();
-//     for (source, target) in mapping {
-//         let source = PathBuf::from(source).canonicalize()?;
-//         let target = PathBuf::from(target);
-//         if args.verbose {
-//             println!(
-//                 "Analyse \n   {}\nand save in \n   {}",
-//                 source.display(),
-//                 target.display()
-//             );
-//         }
-//         let mut buffer = file_reader.open_file(&source)?;
-//         let prepared_content = code_updater.update_code(&mut buffer)?;
-//         let _ = file_writer.write_generated_file(&prepared_content, &target)?;
-//
-//         if args.verbose {
-//             println!(
-//                 "--------------------------------------------------------------------------------"
-//             );
-//             println!("{}", prepared_content);
-//             println!(
-//                 "--------------------------------------------------------------------------------"
-//             );
-//         }
-//     }
-//
-//     Ok(())
-// }
