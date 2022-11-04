d6153a259910e77686a36964b83b98703d2e430f
Daniel Corn <info@cundd.net>
2022-11-04 15:02:57 +0100

    Implement Display for `CommandType`

--- cqrs/src/command/command_type.rs
+++ cqrs/src/command/command_type.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 
 /// The `CommandType` describes the action to perform
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -7,4 +8,15 @@ pub enum CommandType {
     Update,
     Upsert,
     Delete,
+}
+
+impl Display for CommandType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            CommandType::Add => "Add",
+            CommandType::Update => "Update",
+            CommandType::Upsert => "Upsert",
+            CommandType::Delete => "Delete",
+        })
+    }
 }
