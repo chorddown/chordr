094ee4825b3ee047eee43589ff0ef75ed07981c3
Daniel Corn <info@cundd.net>
2023-01-28 16:31:24 +0100

    Cleanup the Catalog Builder benchmark

--- libchordr/benches/catalog_builder_benchmark.rs
+++ libchordr/benches/catalog_builder_benchmark.rs
@@ -1,9 +1,6 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use libchordr::prelude::{CatalogBuilder, FileType};
 use std::path::Path;
-
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-use libchordr::prelude::{CatalogBuilder, Converter, FileType, Format, Formatting, Node};
-use libchordr::test_helpers::{get_test_ast, get_test_metadata};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let songs_dir = format!("{}/tests/resources", env!("CARGO_MANIFEST_DIR"));
