7bfd955880ade1af2201cc5c2c2dea2c27f221c6
Daniel Corn <info@cundd.net>
2025-11-08 22:06:21 +0100

    Allow `update-service-worker.sh` to be called manually

--- webchordr/app/build/update-service-worker.sh
+++ webchordr/app/build/update-service-worker.sh
@@ -1,33 +1,55 @@
 #!/usr/bin/env bash
 
+get_script_dir() {
+    local SOURCE_PATH="${BASH_SOURCE[0]}"
+    local SYMLINK_DIR
+    local SCRIPT_DIR
+    # Resolve symlinks recursively
+    while [ -L "$SOURCE_PATH" ]; do
+        # Get symlink directory
+        SYMLINK_DIR="$(cd -P "$(dirname "$SOURCE_PATH")" >/dev/null 2>&1 && pwd)"
+        # Resolve symlink target (relative or absolute)
+        SOURCE_PATH="$(readlink "$SOURCE_PATH")"
+        # Check if candidate path is relative or absolute
+        if [[ $SOURCE_PATH != /* ]]; then
+            # Candidate path is relative, resolve to full path
+            SOURCE_PATH=$SYMLINK_DIR/$SOURCE_PATH
+        fi
+    done
+    # Get final script directory path from fully resolved source path
+    SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE_PATH")" >/dev/null 2>&1 && pwd)"
+    echo "$SCRIPT_DIR"
+}
+
 if [[ -z ${TRUNK_STAGING_DIR+x} ]]; then
-  echo "[ERROR] \$TRUNK_STAGING_DIR is not set"
-  exit 1
+    BUILD_DIR=$(get_script_dir)"/../dist"
+else
+    BUILD_DIR=${TRUNK_STAGING_DIR}
 fi
 
-cd "$TRUNK_STAGING_DIR" || exit 1
+cd "$BUILD_DIR" || exit 1
 
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-SERVICE_WORKER_FILE=$TRUNK_STAGING_DIR/service-worker.js
+SERVICE_WORKER_FILE=$BUILD_DIR/service-worker.js
 
 RANDOM_ID=$(openssl rand -hex 12)
 perl -i -pe"s+{RANDOM_ID}+$RANDOM_ID+g" "$SERVICE_WORKER_FILE"
 
-JS_PATH=("$TRUNK_STAGING_DIR"/webchordr-*.js)
+JS_PATH=("$BUILD_DIR"/webchordr-*.js)
 JS_FILE_NAME=$(basename "${JS_PATH[@]}")
 if [[ "$JS_FILE_NAME" == "index-*.wasm" ]]; then
-  echo "[ERROR] Could not find file matching 'index-*_bg.wasm'"
-  exit 1
+    echo "[ERROR] Could not find file matching 'index-*_bg.wasm'"
+    exit 1
 fi
 perl -i -pe"s+//{JS}+'/$JS_FILE_NAME',+g" "$SERVICE_WORKER_FILE"
 
-WASM_PATH=("$TRUNK_STAGING_DIR"/webchordr-*_bg.wasm)
+WASM_PATH=("$BUILD_DIR"/webchordr-*_bg.wasm)
 WASM_FILE_NAME=$(basename "${WASM_PATH[@]}")
 if [[ "$WASM_FILE_NAME" == "index-*_bg.wasm" ]]; then
-  echo "[ERROR] Could not find file matching 'index-*_bg.wasm'"
-  exit 1
+    echo "[ERROR] Could not find file matching 'index-*_bg.wasm'"
+    exit 1
 fi
 perl -i -pe"s+//{WASM}+'/$WASM_FILE_NAME',+g" "$SERVICE_WORKER_FILE"
 
@@ -35,6 +57,6 @@ SORTABLE_PATH=(snippets/webchordr-song-list-*/dist/sortable.js)
 SORTABLE_FILE_NAME="${SORTABLE_PATH[*]}"
 perl -i -pe"s+//{SORTABLE}+'/$SORTABLE_FILE_NAME',+g" "$SERVICE_WORKER_FILE"
 
-INDEX_FILE=$TRUNK_STAGING_DIR/index.html
+INDEX_FILE=$BUILD_DIR/index.html
 RANDOM_ID=$(openssl rand -hex 12)
 perl -i -pe"s+{RANDOM_ID}+$RANDOM_ID+g" "$INDEX_FILE"
