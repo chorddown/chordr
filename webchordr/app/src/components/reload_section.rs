58009af618ba102f7d2842cce408ce671861dd05
Daniel Corn <daniel@iresults.li>
2026-08-14 11:59:07 +0200

    Migrate `ReloadSection` into a function component

--- webchordr/app/src/components/reload_section.rs
+++ webchordr/app/src/components/reload_section.rs
@@ -12,48 +12,38 @@ pub struct ReloadSectionProps {
     pub catalog: Option<Rc<Catalog>>,
 }
 
-pub struct ReloadSection {}
+#[component]
+pub fn ReloadSection(ReloadSectionProps { catalog }: &ReloadSectionProps) -> Html {
+    let revision = match catalog {
+        None => "n/a".to_string(),
+        Some(c) => match DateTime::parse_from_rfc2822(&c.revision()) {
+            Ok(d) => d.format("%a %d.%m.%y %H:%M").to_string(),
+            Err(_) => c.revision(),
+        },
+    };
 
-impl Component for ReloadSection {
-    type Message = ();
-    type Properties = ReloadSectionProps;
+    let app_version = format!(
+        "{}-{}",
+        env!("CARGO_PKG_VERSION"),
+        env!("CUNDD_BUILD_REVISION")
+    );
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let revision = match &ctx.props().catalog {
-            None => "n/a".to_string(),
-            Some(c) => match DateTime::parse_from_rfc2822(&c.revision()) {
-                Ok(d) => d.format("%a %d.%m.%y %H:%M").to_string(),
-                Err(_) => c.revision(),
-            },
-        };
-
-        let app_version = format!(
-            "{}-{}",
-            env!("CARGO_PKG_VERSION"),
-            env!("CUNDD_BUILD_REVISION")
-        );
-
-        html! {
-            <div class="reload-section">
-                <div class="reload-button-container">
-                    <a class="reload-button" href="/" title="Reload the song catalog">
-                        <i class="im im-sync"></i>
-                        <Nbsp/>
-                        <span>{"Reload the catalog"}</span>
-                    </a>
-                </div>
-                <div class="reload-section-catalog-revision">
-                    {"Catalog revision: "}{revision}
-                </div>
-
-                <div class="reload-section-version">
-                    {"App version: "}{app_version}
-                </div>
+    html! {
+        <div class="reload-section">
+            <div class="reload-button-container">
+                <a class="reload-button" href="/" title="Reload the song catalog">
+                    <i class="im im-sync"></i>
+                    <Nbsp/>
+                    <span>{"Reload the catalog"}</span>
+                </a>
             </div>
-        }
+            <div class="reload-section-catalog-revision">
+                {"Catalog revision: "}{revision}
+            </div>
+
+            <div class="reload-section-version">
+                {"App version: "}{app_version}
+            </div>
+        </div>
     }
 }
